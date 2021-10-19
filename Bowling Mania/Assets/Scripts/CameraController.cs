f4482281fade98ca792e1076e8c4b20b6187c489
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2021-10-19 20:47:16 +0200

    Add project

---
+++ Bowling Mania/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public bool launched;
+    public Rigidbody cameraRigidBody;
+    void Start()
+    {
+        cameraRigidBody = GetComponent<Rigidbody>();
+        launched = false;
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            launched = true;
+        }
+        cameraMethod(launched);
+    }
+    void cameraMethod(bool boolian)
+    {
+        if (boolian && !Mover.exit)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+        if(Mover.exit)
+        {
+            cameraRigidBody.velocity = new Vector3 (0f, 0f ,0f);
+        }
+    }
+}
---
+++ Bowling Mania/Assets/Scripts/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float Speed;
+    Vector3 mover;
+    public float Strength = 3f;
+    private Rigidbody rb;
+    private bool launched;
+    public GameObject other;
+    public Rigidbody otherRb;
+    public static bool exit;
+    public CameraController cameraController;
+    void Start()
+    {
+        exit = false;
+        rb = GetComponent<Rigidbody>();
+        launched = false;
+    }
+
+    void Update()
+    {
+        if (!launched)
+        {
+            mover = transform.position;
+            mover.x = (Mathf.Sin(7 * Time.time) * Strength);
+            transform.position = mover;
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                launched = true;
+                rb.velocity = transform.forward * Speed;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pin"))
+        {
+            exit = true;
+            (cameraController as Behaviour).enabled = false;
+            StartCoroutine(Delay(4));   
+        }
+
+    }
+    public IEnumerator Delay (float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        other.gameObject.SetActive(false);
+    }
+}
